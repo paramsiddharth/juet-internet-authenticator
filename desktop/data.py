18a1d86b13b0fa9eb8b3e63b5728fe5b694e52df
Param Siddharth <contact@paramsid.com>
2022-01-06 19:07:53 +0530

    Reorganize data
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- desktop/data.py
+++ desktop/data.py
@@ -1,5 +1,9 @@
-test_url = 'https://www.google.com'
+# App metadata
 app_name = 'JUET Internet Authenticator'
+config_file = '.juetrc'
+
+# System-specific
+test_url = 'https://www.google.com'
 desk_url = 'http://10.10.10.1:8090/'
 class routes:
 	login = 'login.xml',
